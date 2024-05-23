2d1b25520f32aa93fb5212ed85b70b1fada1e229
Mjishu <128941980+Mjishu@users.noreply.github.com.>
2024-05-23 16:49:43 -0400

    need to add styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(props){
         <header>
             <Link className="Navbar-link" to="/">Home</Link>
             <Link className="Navbar-link" to="/store">Store</Link>
-            <button className="Navbar-link" onClick={props.toggleCart}>Cart</button>
+            <button className="Navbar-Button" onClick={props.toggleCart}>Cart</button>
             <Link className="Navbar-link" >New item </Link>
         </header>
     )
--- src/components/Shopping.jsx
+++ src/components/Shopping.jsx
@@ -70,7 +70,7 @@ export default function Shopping(props){
 
     if(loading){
         return (
-            <p>Loading...</p>
+            <p className="loading">Loading...</p>
         )
     }
 
